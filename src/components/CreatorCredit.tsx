d7aa2f695d9e0858093156ff7f210348f07cf790
Suraj rai <Surajraiking22@gmail.com>
2026-09-24 17:50:15 +0530

    design: add reusable creator credit

---
+++ src/components/CreatorCredit.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const CreatorCredit: React.FC = () => (
+  <div
+    className="pointer-events-none fixed left-1/2 bottom-[4.75rem] lg:bottom-3 -translate-x-1/2 z-40"
+    aria-label="Created by Suraj Rai"
+  >
+    <div className="rounded-full border border-[#C9973E]/30 bg-[#24180E]/80 px-3 py-1 text-[9px] sm:text-[10px] font-semibold tracking-[0.16em] uppercase text-[#F7D58A] shadow-lg backdrop-blur-md dark:bg-[#120D09]/85">
+      Created by Suraj Rai
+    </div>
+  </div>
+);
