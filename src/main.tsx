052fcea35e94820e20f60674d5dfc4fa3bb64110
Suraj rai <Surajraiking22@gmail.com>
2026-09-26 21:01:32 +0530

    feat: require account before entering app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import {AuthGate} from './components/AuthGate';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthGate />
   </StrictMode>,
 );
